b8208ced667fb3944987da33bd8614e7881df59b
Ritesh Persaud <riteshpersaud2003@gmail.com>
2025-10-24 18:00:36 -0400

    Added dummy data for testing and installed pytest

--- MTTD.py
+++ MTTD.py
@@ -1,1 +1,18 @@
-# Test Case File
+from MRTD import viz_encoder
+
+
+TEST_DATA = {
+    "document_type": "P<",
+    "country_code": "CAN",
+    "lastname": "DOE",
+    "given_name": "JOHN MICHAEL",
+    "passport_number": "AB1234567",
+    "nationality": "CAN",
+    "date_of_birth": "900101", 
+    "gender": "M",
+    "expiration_date": "300101",
+    "personal_number": "9876543210"
+}
+
+LINE_1_TEST = "P<<CANDOE<<JOHN<MICHAEL<<<<<<<<<<<<<<<<<<<<<"
+LINE_2_TEST = "AB12345676CAN9001010M30010189876543210<<<<<5"
---
+++ README.md
@@ -0,0 +1,21 @@
+# Project Setup with Pipenv
+
+This project uses Pipenv to manage its Python virtual environment and dependencies.
+
+## Creating the Virtual Environment and Installing pytest
+
+- Create virtual environment and install pytest as a dev dependency:
+
+```bash
+pipenv install --dev pytest
+```
+- Activate the virtual environment shell:
+
+```bash
+pipenv shell
+```
+- Deactivate shell when done:
+
+```bash
+exit
+```
